9d9052b7b13f93c5e2918928060f1083b7744c2c
MikeCoder <mike@mikecoder.cn>
2017-12-04 13:07:15 +0800

    add json preview

--- mjsonviewer.js
+++ mjsonviewer.js
@@ -62,7 +62,8 @@ function onGot(result) {
         body = document.body,
         first = body && body.firstChild,
         mod = /Mac|iPod|iPhone|iPad|Pike/.test(navigator.platform) ? "metaKey" : "ctrlKey",
-        rand = Math.random().toString(36).slice(2),
+        // rand = Math.random().toString(36).slice(2),
+        rand = 1;
         HOV = "H" + rand,
         DIV = "D" + rand,
         KEY = "K" + rand,
@@ -335,5 +336,93 @@ function onGot(result) {
     })
 }
 
+function formatJson(json, options) {
+    var reg = null,
+        formatted = '',
+        pad = 0,
+        PADDING = '    ';
+    options = options || {};
+    options.newlineAfterColonIfBeforeBraceOrBracket = (options.newlineAfterColonIfBeforeBraceOrBracket === true) ? true : false;
+    options.spaceAfterColon = (options.spaceAfterColon === false) ? false : true;
+    if (typeof json !== 'string') {
+        json = JSON.stringify(json);
+    } else {
+        json = JSON.parse(json);
+        json = JSON.stringify(json);
+    }
+    reg = /([\{\}])/g;
+    json = json.replace(reg, '\r\n$1\r\n');
+    reg = /([\[\]])/g;
+    json = json.replace(reg, '\r\n$1\r\n');
+    reg = /(\,)/g;
+    json = json.replace(reg, '$1\r\n');
+    reg = /(\r\n\r\n)/g;
+    json = json.replace(reg, '\r\n');
+    reg = /\r\n\,/g;
+    json = json.replace(reg, ',');
+    if (!options.newlineAfterColonIfBeforeBraceOrBracket) {
+        reg = /\:\r\n\{/g;
+        json = json.replace(reg, ':{');
+        reg = /\:\r\n\[/g;
+        json = json.replace(reg, ':[');
+    }
+    if (options.spaceAfterColon) {
+        reg = /\:/g;
+        json = json.replace(reg, ':');
+    }(json.split('\r\n')).forEach(function (node, index) {
+        var i = 0,
+            indent = 0,
+            padding = '';
+        if (node.match(/\{$/) || node.match(/\[$/)) {
+            indent = 1;
+        } else if (node.match(/\}/) || node.match(/\]/)) {
+            if (pad !== 0) {
+                pad -= 1;
+            }
+        } else {
+            indent = 0;
+        }
+        for (i = 0; i < pad; i++) {
+            padding += PADDING;
+        }
+        formatted += padding + node + '\r\n';
+        pad += indent;
+    }
+    );
+    return formatted;
+};
+
+function isJSON(str) {
+    if (typeof str == 'string') {
+        try {
+            var obj = JSON.parse(str);
+            if (str.indexOf('{') > - 1) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (e) {
+            return false;
+        }
+    }
+    return false;
+}
+
+function onParse() {
+    var items = document.getElementsByClassName('S1');
+    for (var i = 0; i < items.length; i++) {
+        try {
+            var json = (eval(items[i].innerHTML));
+            if (isJSON(json)) {
+                var formated = formatJson(json);
+                console.log(formated);
+                items[i].onclick = function() {
+                    alert(formated);
+                }
+            }
+        } catch (e) {}
+    }
+}
+
 var getting = browser.storage.local.get();
-getting.then(onGot, onError);
+getting.then(onGot, onError).then(onParse, onError);
