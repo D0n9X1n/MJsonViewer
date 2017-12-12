26f59561523dcf859182f95fae174464045062da
MikeCoder <mike@mikecoder.cn>
2017-12-12 20:36:56 +0800

    update publish.sh

--- publish.sh
+++ publish.sh
@@ -4,6 +4,25 @@ if [[ -f ../MJsonViewer.zip ]]; then
     rm ../MJsonViewer.zip
 fi
 
-rm -rf .DS_Store
+if [[ -f .DS_Store ]]; then
+    rm -rf .DS_Store
+fi
+
+if [[ -f images/.DS_Store ]]; then
+    rm -rf images/.DS_Store
+fi
+
+if [[ -f options/.DS_Store ]]; then
+    rm -rf options/.DS_Store
+fi
+
+if [[ -f icons/.DS_Store ]]; then
+    rm -rf icons/.DS_Store
+fi
+
+if [[ -f scripts/.DS_Store ]]; then
+    rm -rf scripts/.DS_Store
+fi
+
 zip -r ../MJsonViewer.zip *
 zip -d ../MJsonViewer.zip publish.sh
